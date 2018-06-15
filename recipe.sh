4b206f9412e3b18233e213c350e305f640af4dda
Yurii Maravin <yurii.maravin@cern.ch>
2018-06-14 20:04:09 -0500

    Adding missing so library copy to the release area for condor submission to work properly

--- recipe.sh
+++ recipe.sh
@@ -2,5 +2,5 @@ pushd $CMSSW_BASE/src
 
 git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
 (cd ZZMatrixElement ; git checkout -b v2.1.7b1 ; . setup.sh -j 12)
-cp "$CMSSW_BASE/src/ZZMatrixElement/MELA/data/$SCRAM_ARCH/libmcfm_705.so" "$CMSSW_BASE/lib/$SCRAM_ARCH/libmcfm_705.so"
+cp "$CMSSW_BASE/src/ZZMatrixElement/MELA/data/$SCRAM_ARCH/"*so "$CMSSW_BASE/lib/$SCRAM_ARCH/"
 popd
