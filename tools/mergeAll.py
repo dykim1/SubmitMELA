f8226b2d427f3de438d8300b472bf3998d35c0e2
Yurii Maravin <yurii.maravin@cern.ch>
2018-06-18 21:27:45 -0500

    Automatic script that merges files together

---
+++ tools/mergeAll.py
@@ -0,0 +1,39 @@
+# code to merge the output from running MELA on svFitted samples by Doyeong. 
+import os, re, sys
+
+directory = sys.argv[1]
+dir = os.popen("ls " + directory + "/*")
+lines = dir.readlines()
+
+datasets = []
+for line in lines:
+    elements = re.split("-", line)
+    file = ""
+    if len(re.split("TauTau_13",elements[len(elements)-2])) > 1:
+        file = elements[-1]
+    else:
+        file = elements[-2] + "-" + elements[-1]
+    element = re.split("\_", file)[0]
+    if not element in datasets:
+        print elements
+        datasets += [element,]
+
+print datasets
+
+
+for iSet in datasets:
+    if iSet <> "Tbar-tW":
+        continue
+
+    listOfFiles = []
+    dir = os.popen("ls " + directory + "/*" + iSet + "*root")
+    files = dir.readlines()
+    for iFile in files:
+        if len(re.split("-" + iSet + "_", iFile)) > 1:
+            listOfFiles += [iFile[:-1],]
+    command = "hadd " + iSet + "_svFit_MELA.root " 
+    for iFile in listOfFiles:
+        command  += iFile + " "
+    #os.system(command)
+    print command
+
